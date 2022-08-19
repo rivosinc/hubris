dca31f76ae5c80f6e2f701eb810eeecf99ff6ee1
Furquan Shaikh <furquan@rivosinc.com>
2022-08-18 21:56:56 -0700

    Add demo-rv64-qemu-virt
    This change adds a demo app for rv64 qemu virt machine. Most of it is
    copied over from demo-hifive-inventor.

---
+++ app/demo-rv64-qemu-virt/src/main.rs
@@ -0,0 +1,19 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+extern crate riscv_rt;
+
+use riscv_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    // TODO(furquan): This is copied over from hifive-inventor. Fix?
+    const CYCLES_PER_MS: u32 = 8_000;
+
+    unsafe { kern::startup::start_kernel(CYCLES_PER_MS) }
+}
