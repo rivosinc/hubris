ddaeb40eb2640a9dac73399a425e9002c08aca35
Fawaz Tirmizi <fawaz@rivosinc.com>
2022-09-08 15:32:19 -0700

    Added MutablePerfectHashMap and new functions
    `MutablePerfectHashMap` is identical to `PerfectHashMap` except for the
    fact that it has a new method: `set()`. This will write to the value
    associated with the given key if there is already a value, otherwise
    it will return an error.
    
    This also adds the `contains()` method to both `PerfectHashMap` and
    `MutablePerfectHashMap`. This method will indicate if there exists a
    value for the given key.

--- lib/phash/src/lib.rs
+++ lib/phash/src/lib.rs
@@ -34,6 +34,58 @@ impl<'a, K: Copy + PerfectHash + PartialEq, V> PerfectHashMap<'a, K, V> {
         }
     }
 
+    #[inline(always)]
+    pub fn contains(&self, key: K) -> bool {
+        self.get(key).is_some()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &(K, V)> {
+        self.values.iter()
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+pub struct MutablePerfectHashMap<'a, K, V> {
+    pub m: u32,
+    pub values: &'a mut [(K, V)],
+}
+
+impl<'a, K: Copy + PerfectHash + PartialEq, V> MutablePerfectHashMap<'a, K, V> {
+    /// Looks up a value in the table by key, returning `None` if the key was
+    /// not stored in the table.
+    #[inline(always)]
+    pub fn get(&self, key: K) -> Option<&V> {
+        if self.values.is_empty() {
+            return None;
+        }
+        let i = key.phash(self.m) % self.values.len();
+        if key == self.values[i].0 {
+            Some(&self.values[i].1)
+        } else {
+            None
+        }
+    }
+
+    #[inline(always)]
+    pub fn set(&mut self, key: K, val: V) -> Result<(), ()> {
+        if self.values.is_empty() {
+            return Err(());
+        }
+        let i = key.phash(self.m) % self.values.len();
+        if key == self.values[i].0 {
+            self.values[i].1 = val;
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    #[inline(always)]
+    pub fn contains(&self, key: K) -> bool {
+        self.get(key).is_some()
+    }
+
     pub fn iter(&self) -> impl Iterator<Item = &(K, V)> {
         self.values.iter()
     }
