b82ccaa38a8b456c0a351d0296cf8c3a7dac61c1
Andrew Zoghby <azoghby@rivosinc.com>
2022-09-14 15:58:37 -0700

    chips: add opentitan
    Co-authored-by: Tyler Ng <tkng@rivosinc.com>

---
+++ chips/opentitan/qemu.sh
@@ -0,0 +1,1 @@
+qemu-system-riscv32 -nographic -M opentitan -global ibex-timer.timebase-freq=1000000 -device loader,addr=0x20000000,cpu-num=0,file=final.bin -semihosting -semihosting-config enable=on,userspace=on
