66492e3eca6ca45a52a1fdff754adbc7c29eecde
Andrew Zoghby <azoghby@rivosinc.com>
2022-09-14 16:15:30 -0700

    app: demo: add `demo-opentitan-earlgrey`.
    This demo utilizes the opentitan AON.
    
    Co-authored-by: Tyler Ng <tkng@rivosinc.com>

---
+++ app/demo-opentitan-earlgrey/src/main.rs
@@ -0,0 +1,37 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+extern crate riscv_rt;
+
+use riscv_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    // The opentitan requires enabling the timer and interrupts
+    // for mtime and mtimecmp to behave as expected
+    // See https://docs.opentitan.org/hw/ip/rv_timer/doc/
+    const OT_TIMER_CTRL: u32 = 0x4010_0004;
+    const OT_TIMER_INTR: u32 = 0x4010_0100;
+
+    // Enable mtime to count up
+    let timer = OT_TIMER_CTRL as *mut u32;
+    unsafe {
+        *timer |= 1;
+    };
+
+    // Enable interrupts when mtime passes mtimecmp
+    let intr = OT_TIMER_INTR as *mut u32;
+    unsafe {
+        *intr |= 1;
+    };
+
+    // TODO this will need to change when run on actual hardware
+    const MTIMECMP: u32 = 1000;
+
+    unsafe { kern::startup::start_kernel(MTIMECMP) }
+}
