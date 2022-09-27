0290fd0dba9e2430dd88c8ea4942eec7633f08c1
Fawaz Tirmizi <fawaz@rivosinc.com>
2022-09-27 14:38:36 -0700

    Updated FE310-RTC driver to use new periph consts
    Now that `build_util` can provide constants for peripherals, there is no
    need to hardcode the address in the driver.

---
+++ drv/fe310-rtc/build.rs
@@ -0,0 +1,19 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = {
+        let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+        File::create(out.join("rtc_config.rs")).unwrap()
+    };
+
+    writeln!(file, "{}", build_util::task_peripherals_str())?;
+
+    return Ok(());
+}
