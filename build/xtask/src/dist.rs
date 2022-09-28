d620139282628827b48f38e4ec3a1921c089a467
Furquan Shaikh <furquan@rivosinc.com>
2022-09-27 19:42:44 -0700

    xtask: Retain linker scripts `memory.x` generated for task linking
    This change retains `memory.x` that is generated in the final step
    of task linking into final image. This generated file for each task
    can be found under ${DIST_DIR}/${TASK_NAME}-memory.x

--- build/xtask/src/dist.rs
+++ build/xtask/src/dist.rs
@@ -870,6 +870,13 @@ fn link_task(
         image_name,
     )
     .context(format!("failed to generate linker script for {}", name))?;
+
+    let working_dir = &cfg.dist_dir;
+    fs::copy(
+        "target/memory.x",
+        working_dir.join(format!("{}-memory.x", name)),
+    )?;
+
     fs::copy(&cfg.arch_consts.link_script, "target/link.x")?;
     if cfg.toml.need_tz_linker(name) {
         fs::copy("build/trustzone.x", "target/trustzone.x")?;
@@ -907,6 +914,13 @@ fn link_dummy_task(cfg: &PackageConfig, name: &str) -> Result<()> {
         &cfg.toml.image_names[0],
     )
     .context(format!("failed to generate linker script for {}", name))?;
+
+    let working_dir = &cfg.dist_dir;
+    fs::copy(
+        "target/memory.x",
+        working_dir.join(format!("{}-memory.x", name)),
+    )?;
+
     fs::copy(cfg.arch_consts.tlink_script, "target/link.x")?;
     if cfg.toml.need_tz_linker(name) {
         fs::copy("build/trustzone.x", "target/trustzone.x")?;
