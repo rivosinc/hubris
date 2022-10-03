5c78e8db8075bb5addabe23657234e4995c401cc
Andrew Zoghby <azoghby@rivosinc.com>
2022-10-03 11:53:02 -0700

    app: demos: add demo for ibex-super-system.
    This is useful for verifing hubris functionality on actual hardware.

---
+++ app/demo-ibex-super-system/src/main.rs
@@ -0,0 +1,18 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+extern crate riscv_rt;
+
+use riscv_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    const CYCLES_PER_MS: u32 = 0x60_0000;
+
+    unsafe { kern::startup::start_kernel(CYCLES_PER_MS) }
+}
