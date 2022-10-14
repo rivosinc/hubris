7e2c980444ab7a5b59a4888c3f71d0bd9efc9d17
Tom Dewey <tdewey@rivosinc.com>
2022-10-14 18:08:35 +0100

    sys/kern: Add RV64 Supervisor support
    Using rust macros and name aliasing, change references to
    m-registers to refer to x-register names instead, and then alias
    these as s- or m- registers based on a new feature flag:
    - riscv-supervisor-mode
    
    Signed-off-by: Tom Dewey <tdewey@rivosinc.com>

--- sys/kern/src/arch/rv64/trap.rs
+++ sys/kern/src/arch/rv64/trap.rs
@@ -11,13 +11,148 @@ use crate::startup::with_task_table;
 use crate::task;
 use abi::{FaultInfo, FaultSource};
 
-extern crate riscv_rt;
+//use riscv::register;
+#[cfg(feature = "riscv-supervisor-mode")]
+use riscv::register::{
+    scause as xcause, scause::Exception as xcauseException,
+    scause::Interrupt as xcauseInterrupt,
+    scause::Interrupt::SupervisorTimer as xInterruptTimer,
+    scause::Trap as xcauseTrap, sepc as xepc, sscratch as xscratch,
+    stval as xtval, stvec as xtvec, stvec::TrapMode as xTrapMode,
+};
 
-use riscv::register;
-use riscv::register::mcause::{Exception, Interrupt, Trap};
+#[cfg(not(feature = "riscv-supervisor-mode"))]
+use riscv::register::{
+    mcause as xcause, mcause::Exception as xcauseException,
+    mcause::Interrupt as xcauseInterrupt,
+    mcause::Interrupt::SupervisorTimer as xInterruptTimer,
+    mcause::Trap as xcauseTrap, mepc as xepc, mscratch as xscratch,
+    mtval as xtval, mtvec as xtvec, mtvec::TrapMode as xTrapMode,
+};
 
 use crate::arch::get_current_task;
 use crate::arch::{incr_ticks, reset_timer};
+
+macro_rules! start_trap_fn_common {
+    ($scratch_reg:literal, $epc_reg:literal, $return_call:literal) => {
+        unsafe {
+            asm!(
+                "
+                #
+                # Store full task status on entry, setting up a0 to point at our
+                # current task so that it's passed into our exception handler.
+                #
+                # xscratch temporarily doesn't point to current task since it is
+                # used to stash a0. mscratch is restored to current task pointer
+                # just before jump to trap_handler.
+                #
+                ",
+                concat!("csrrw a0, ", $scratch_reg, ", a0"),
+                "
+                sd ra,   0*8(a0)
+                sd sp,   1*8(a0)
+                sd gp,   2*8(a0)
+                sd tp,   3*8(a0)
+                sd t0,   4*8(a0)
+                sd t1,   5*8(a0)
+                sd t2,   6*8(a0)
+                sd s0,   7*8(a0)
+                sd s1,   8*8(a0)
+                #sd a0,  9*8(a0)
+                sd a1,  10*8(a0)
+                sd a2,  11*8(a0)
+                sd a3,  12*8(a0)
+                sd a4,  13*8(a0)
+                sd a5,  14*8(a0)
+                sd a6,  15*8(a0)
+                sd a7,  16*8(a0)
+                sd s2,  17*8(a0)
+                sd s3,  18*8(a0)
+                sd s4,  19*8(a0)
+                sd s5,  20*8(a0)
+                sd s6,  21*8(a0)
+                sd s7,  22*8(a0)
+                sd s8,  23*8(a0)
+                sd s9,  24*8(a0)
+                sd s10, 25*8(a0)
+                sd s11, 26*8(a0)
+                sd t3,  27*8(a0)
+                sd t4,  28*8(a0)
+                sd t5,  29*8(a0)
+                sd t6,  30*8(a0)
+                ",
+                concat!("csrr a1, ", $scratch_reg),
+                "
+                sd a1,  31*8(a0)    # store xepc for resume
+                ",
+                concat!("csrrw a1, ", $scratch_reg, ", a0   # current task ptr restored in xscratch"),
+                "
+                sd a1, 9*8(a0)      # store a0 itself
+
+                #
+                # Jump to our main rust handler
+                #
+                jal trap_handler
+
+                #
+                # On the way out we may have switched to a different task, load
+                # everything in and resume (using t6 as it's resored last).
+                #
+                ",
+                concat!("csrr t6, ", $scratch_reg),
+                "
+                ld t5,  31*8(t6)     # restore xepc
+                ",
+                concat!("csrw ", $epc_reg, ", t5"),
+                "
+
+                ld ra,   0*8(t6)
+                ld sp,   1*8(t6)
+                ld gp,   2*8(t6)
+                ld tp,   3*8(t6)
+                ld t0,   4*8(t6)
+                ld t1,   5*8(t6)
+                ld t2,   6*8(t6)
+                ld s0,   7*8(t6)
+                ld s1,   8*8(t6)
+                ld a0,   9*8(t6)
+                ld a1,  10*8(t6)
+                ld a2,  11*8(t6)
+                ld a3,  12*8(t6)
+                ld a4,  13*8(t6)
+                ld a5,  14*8(t6)
+                ld a6,  15*8(t6)
+                ld a7,  16*8(t6)
+                ld s2,  17*8(t6)
+                ld s3,  18*8(t6)
+                ld s4,  19*8(t6)
+                ld s5,  20*8(t6)
+                ld s6,  21*8(t6)
+                ld s7,  22*8(t6)
+                ld s8,  23*8(t6)
+                ld s9,  24*8(t6)
+                ld s10, 25*8(t6)
+                ld s11, 26*8(t6)
+                ld t3,  27*8(t6)
+                ld t4,  28*8(t6)
+                ld t5,  29*8(t6)
+                ld t6,  30*8(t6)
+                ",
+                $return_call,
+                options(noreturn), // Mandatory for naked functions
+            )
+        }
+    }
+}
+
+macro_rules! start_trap_fn {
+    (supervisor) => {
+        start_trap_fn_common!("sscratch", "sepc", "sret")
+    };
+    (machine) => {
+        start_trap_fn_common!("mscratch", "mepc", "mret")
+    };
+}
 
 // Provide our own interrupt vector to handle save/restore of the task on
 // entry, overwriting the symbol set up by riscv-rt.  The repr(align(4)) is
@@ -31,105 +166,7 @@ use crate::arch::{incr_ticks, reset_timer};
 #[link_section = ".trap.rust"]
 #[export_name = "_start_trap"]
 pub unsafe extern "C" fn _start_trap() {
-    unsafe {
-        asm!(
-            "
-        #
-        # Store full task status on entry, setting up a0 to point at our
-        # current task so that it's passed into our exception handler.
-        #
-        # mscratch temporarily doesn't point to current task since it is
-        # used to stash a0. mscratch is restored to current task pointer
-        # just before jump to trap_handler.
-        #
-        csrrw a0, mscratch, a0    # mscratch no longer holds current task ptr
-        sd ra,   0*8(a0)
-        sd sp,   1*8(a0)
-        sd gp,   2*8(a0)
-        sd tp,   3*8(a0)
-        sd t0,   4*8(a0)
-        sd t1,   5*8(a0)
-        sd t2,   6*8(a0)
-        sd s0,   7*8(a0)
-        sd s1,   8*8(a0)
-        #sd a0,  9*8(a0)
-        sd a1,  10*8(a0)
-        sd a2,  11*8(a0)
-        sd a3,  12*8(a0)
-        sd a4,  13*8(a0)
-        sd a5,  14*8(a0)
-        sd a6,  15*8(a0)
-        sd a7,  16*8(a0)
-        sd s2,  17*8(a0)
-        sd s3,  18*8(a0)
-        sd s4,  19*8(a0)
-        sd s5,  20*8(a0)
-        sd s6,  21*8(a0)
-        sd s7,  22*8(a0)
-        sd s8,  23*8(a0)
-        sd s9,  24*8(a0)
-        sd s10, 25*8(a0)
-        sd s11, 26*8(a0)
-        sd t3,  27*8(a0)
-        sd t4,  28*8(a0)
-        sd t5,  29*8(a0)
-        sd t6,  30*8(a0)
-        csrr a1, mepc
-        sd a1,  31*8(a0)    # store mepc for resume
-        csrrw a1, mscratch, a0        # current task ptr restored in mscratch
-        sd a1, 9*8(a0)      # store a0 itself
-
-        #
-        # Jump to our main rust handler
-        #
-        jal trap_handler
-
-        #
-        # On the way out we may have switched to a different task, load
-        # everything in and resume (using t6 as it's resored last).
-        #
-        csrr t6, mscratch
-
-        ld t5,  31*8(t6)     # restore mepc
-        csrw mepc, t5
-
-        ld ra,   0*8(t6)
-        ld sp,   1*8(t6)
-        ld gp,   2*8(t6)
-        ld tp,   3*8(t6)
-        ld t0,   4*8(t6)
-        ld t1,   5*8(t6)
-        ld t2,   6*8(t6)
-        ld s0,   7*8(t6)
-        ld s1,   8*8(t6)
-        ld a0,   9*8(t6)
-        ld a1,  10*8(t6)
-        ld a2,  11*8(t6)
-        ld a3,  12*8(t6)
-        ld a4,  13*8(t6)
-        ld a5,  14*8(t6)
-        ld a6,  15*8(t6)
-        ld a7,  16*8(t6)
-        ld s2,  17*8(t6)
-        ld s3,  18*8(t6)
-        ld s4,  19*8(t6)
-        ld s5,  20*8(t6)
-        ld s6,  21*8(t6)
-        ld s7,  22*8(t6)
-        ld s8,  23*8(t6)
-        ld s9,  24*8(t6)
-        ld s10, 25*8(t6)
-        ld s11, 26*8(t6)
-        ld t3,  27*8(t6)
-        ld t4,  28*8(t6)
-        ld t5,  29*8(t6)
-        ld t6,  30*8(t6)
-
-        mret
-        ",
-            options(noreturn),
-        );
-    }
+    start_trap_fn!(supervisor);
 }
 
 #[no_mangle]
@@ -182,26 +219,26 @@ fn timer_handler() {
 //
 #[no_mangle]
 fn trap_handler(task: &mut task::Task) {
-    let cause = register::mcause::read().cause();
+    let cause = xcause::read().cause();
     match cause {
         //
         // Interrupts.  Only our periodic MachineTimer interrupt via mtime is
         // supported at present.
         //
-        Trap::Interrupt(Interrupt::MachineTimer) => {
+        xcauseTrap::Interrupt(xInterruptTimer) => {
             timer_handler();
         }
         //
         // System Calls.
         //
-        Trap::Exception(Exception::UserEnvCall) => {
+        xcauseTrap::Exception(xcauseException::UserEnvCall) => {
             unsafe {
                 // Advance program counter past ecall instruction.
                 // This path handles the ecall instruction only and
-                // so the mepc is advanced by 4. For other paths,
+                // so the xepc is advanced by 4. For other paths,
                 // that deal with compressed instructions will have to
                 // adjust this accordingly.
-                let epc = register::mepc::read() as u64 + 4;
+                let epc = xepc::read() as u64 + 4;
                 let saved_state = task.save_mut();
 
                 saved_state.set_pc(epc);
@@ -214,19 +251,19 @@ fn trap_handler(task: &mut task::Task) {
         //
         // Exceptions.  Routed via the most appropriate FaultInfo.
         //
-        Trap::Exception(Exception::IllegalInstruction) => unsafe {
+        xcauseTrap::Exception(xcauseException::IllegalInstruction) => unsafe {
             handle_fault(task, FaultInfo::IllegalInstruction);
         },
-        Trap::Exception(Exception::LoadFault) => unsafe {
+        xcauseTrap::Exception(xcauseException::LoadFault) => unsafe {
             handle_fault(
                 task,
                 FaultInfo::MemoryAccess {
-                    address: Some(register::mtval::read() as usize),
+                    address: Some(xtval::read() as usize),
                     source: FaultSource::User,
                 },
             );
         },
-        Trap::Exception(Exception::InstructionFault) => unsafe {
+        xcauseTrap::Exception(xcauseException::InstructionFault) => unsafe {
             handle_fault(task, FaultInfo::IllegalText);
         },
         _ => {
@@ -237,7 +274,7 @@ fn trap_handler(task: &mut task::Task) {
 
 #[no_mangle]
 unsafe fn handle_fault(task: *mut task::Task, fault: FaultInfo) {
-    // Safety: we're dereferencing the current taask pointer, which we're
+    // Safety: we're dereferencing the current task pointer, which we're
     // trusting the restof this module to maintain correctly.
     let idx = usize::from(unsafe { (*task).descriptor().index });
     unsafe {
