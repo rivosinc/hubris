2502947f7569b470e5a7efcb3dda0d9bbf081da4
Tom Dewey <tdewey@rivosinc.com>
2022-10-20 16:15:56 +0100

    supervisor: Use SBI Set Timer call for timers

--- sys/kern/src/arch/rv64/stimer.rs
+++ sys/kern/src/arch/rv64/stimer.rs
@@ -2,11 +2,18 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use crate::arch::sbi_set_timer;
+
 #[no_mangle]
 pub unsafe fn set_timer(tick_divisor: u32) {
-    // TODO: SBI Call
+    // TODO: Feature detection for Sstc, SBI call for non-supporting, supporting get:
+    //       riscv::register::stime::write(0);
+    //       riscv::register::stimecmp::write(tick_divisor.into());
+    sbi_set_timer(tick_divisor.into());
 }
 
 pub fn reset_timer() {
-    // TODO: SBI Call
+    // TODO: Feature detection for Sstc, SBI call for non-supporting, supporting get:
+    //       riscv::register::stime::write(0);
+    sbi_set_timer(0);
 }
