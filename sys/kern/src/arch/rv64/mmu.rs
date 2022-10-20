3731c556cdcd6691bb0e92b4d4e75d26042bb977
Tom Dewey <tdewey@rivosinc.com>
2022-10-20 16:13:43 +0100

    supervisor: Fix warning on unused argument in mmu.rs

--- sys/kern/src/arch/rv64/mmu.rs
+++ sys/kern/src/arch/rv64/mmu.rs
@@ -3,8 +3,7 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use crate::task;
-use riscv::register;
 
-pub fn apply_memory_protection(task: &task::Task) {
+pub fn apply_memory_protection(_task: &task::Task) {
     // TODO: Apply protection via S-mode accessible instructions
 }
