3924b1d9db03c7ded5ac1accd6a6479b67a0008e
Richard Oliver <richard@rivosinc.com>
2022-11-02 15:22:51 +0000

    RV32: Handle StoreFault

--- sys/kern/src/arch/riscv32.rs
+++ sys/kern/src/arch/riscv32.rs
@@ -571,7 +571,8 @@ fn trap_handler(task: &mut task::Task) {
         Trap::Exception(Exception::IllegalInstruction) => unsafe {
             handle_fault(task, FaultInfo::IllegalInstruction);
         },
-        Trap::Exception(Exception::LoadFault) => unsafe {
+        Trap::Exception(Exception::LoadFault)
+        | Trap::Exception(Exception::StoreFault) => unsafe {
             handle_fault(
                 task,
                 FaultInfo::MemoryAccess {
