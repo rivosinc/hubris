56a093f2f59594470789eae3f82609b9104d871d
Fawaz Tirmizi <fawaz@rivosinc.com>
2022-11-17 15:01:10 -0800

    `call_rustfmt`: Handle cases without `rustup`

--- build/call_rustfmt/src/lib.rs
+++ build/call_rustfmt/src/lib.rs
@@ -5,24 +5,29 @@
 use anyhow::{bail, Result};
 use std::path::Path;
 use std::process::Command;
+use std::string::String;
 
 /// Rewrites a file in-place using rustfmt.
 ///
 /// Rustfmt likes to rewrite files in-place. If this concerns you, copy your
 /// important file to a temporary file, and then call this on it.
 pub fn rustfmt(path: impl AsRef<Path>) -> Result<()> {
-    let which_out =
-        Command::new("rustup").args(["which", "rustfmt"]).output()?;
+    let mut cmd =
+        match Command::new("rustup").args(["which", "rustfmt"]).output() {
+            Ok(out) => {
+                if !out.status.success() {
+                    bail!("rustup which returned status {}", out.status);
+                } else {
+                    Command::new(String::from_utf8(out.stdout)?.trim())
+                }
+            }
+            Err(_) => match Command::new("rustfmt").output() {
+                Ok(_) => Command::new("rustfmt"),
+                Err(_) => bail!("No rustfmt available"),
+            },
+        };
 
-    if !which_out.status.success() {
-        bail!("rustup which returned status {}", which_out.status);
-    }
-
-    let out_str = std::str::from_utf8(&which_out.stdout)?.trim();
-
-    println!("will invoke: {}", out_str);
-
-    let fmt_status = Command::new(out_str).arg(path.as_ref()).status()?;
+    let fmt_status = cmd.arg(path.as_ref()).status()?;
     if !fmt_status.success() {
         bail!("rustfmt returned status {}", fmt_status);
     }
