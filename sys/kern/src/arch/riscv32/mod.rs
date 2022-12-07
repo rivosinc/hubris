4bdfd578107e0c3f90c65fa88b732f510d328494
Andrew Zoghby <azoghby@rivosinc.com>
2022-12-07 15:40:27 -0800

    sys: kern: arch: rv32: break mtimer into its own module.

--- sys/kern/src/arch/riscv32/mod.rs
+++ sys/kern/src/arch/riscv32/mod.rs
@@ -57,6 +57,9 @@ macro_rules! uassert {
 mod pmp;
 pub use pmp::*;
 
+mod mtimer;
+pub use mtimer::*;
+
 /// On RISC-V we use a global to record the current task pointer.  It may be
 /// possible to use the mscratch register instead.
 #[no_mangle]
@@ -65,7 +68,7 @@ static mut CURRENT_TASK_PTR: Option<NonNull<task::Task>> = None;
 /// To allow our clock frequency to be easily determined from a debugger, we
 /// store it in memory.
 #[no_mangle]
-static mut CLOCK_FREQ_KHZ: u32 = 0;
+pub static mut CLOCK_FREQ_KHZ: u32 = 0;
 
 /// RISC-V volatile registers that must be saved across context switches.
 #[repr(C)]
@@ -589,32 +592,6 @@ unsafe fn handle_fault(task: *mut task::Task, fault: FaultInfo) {
             // until next kernel entry, so we meet the function requirements.
             crate::task::activate_next_task(next);
         });
-    }
-}
-
-// Timer handling.
-//
-// We currently only support single HART systems.  From reading elsewhere,
-// additional harts have their own mtimecmp offset at 0x8 intervals from hart0.
-//
-// Configure the timer.
-//
-// RISC-V Privileged Architecture Manual
-// 3.2.1 Machine Timer Registers (mtime and mtimecmp)
-//
-unsafe fn reset_timer() {
-    //
-    // Increase mtimecmp for the next interrupt
-    // This will also clear the pending timer interrupt.
-    //
-    unsafe {
-        let mut mtimecmp =
-            core::ptr::read_volatile(crate::startup::MTIMECMP as *mut u64);
-        mtimecmp += CLOCK_FREQ_KHZ as u64;
-        core::ptr::write_volatile(
-            crate::startup::MTIMECMP as *mut u64,
-            mtimecmp,
-        );
     }
 }
 
