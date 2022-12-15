e318981022ae583fa8832098ab05b32b5820e760
Andrew Zoghby <azoghby@rivosinc.com>
2022-12-15 09:11:18 -0800

    nix: add port option to qemu runner
    This allows multiple runners to be started concurrently.  If no port is
    specified, it will omit the parameter and allow humility to choose the
    default.
    
    This is a cherry-pick of https://gitlab.ba.rivosinc.com/rv/sw/ext/hubris/-/merge_requests/52

--- nix/qemu-test-suite.nix
+++ nix/qemu-test-suite.nix
@@ -1,21 +1,25 @@
 {
+  lib,
   runCommand,
   hubris,
   humility,
   qemu,
-}:
-runCommand ("hubris-qemu-tests-" + hubris.app) {}
-''
-  echo "running hubris test suite for: " ${hubris.app}
+  port ? null,
+}: let
+  port_string = lib.optionalString (lib.isString port) "-p ${port}";
+in
+  runCommand ("hubris-qemu-tests-" + hubris.app) {}
+  ''
+    echo "running hubris test suite for: " ${hubris.app}
 
-  # prepare environment
-  mkdir -p $out
-  export PATH=${qemu}/bin/:$PATH
-  export HUMILITY_ARCHIVE=${hubris}/build-${hubris.app}.zip
+    # prepare environment
+    mkdir -p $out
+    export PATH=${qemu}/bin/:$PATH
+    export HUMILITY_ARCHIVE=${hubris}/build-${hubris.app}.zip
 
-  # we expect this to timeout, so ensure it always gives a good return value
-  timeout -k 1s 10s ${humility}/bin/humility qemu | tee $out/qemu.log || true
+    # we expect this to timeout, so ensure it always gives a good return value
+    timeout -k 1s 10s ${humility}/bin/humility qemu ${port_string} | tee $out/qemu.log || true
 
-  grep "done pass" $out/qemu.log
-  echo "test suite passed"
-''
+    grep "done pass" $out/qemu.log
+    echo "test suite passed"
+  ''
